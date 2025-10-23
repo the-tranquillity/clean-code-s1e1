ce003306e48100ae38f2296e575fee33ec693f81
the-tranquillity <katyanov@protonmail.com>
2025-10-24 01:25:54 +0800

    fix: fix checkbox class in js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    editInput.className="task-item__checkbox";
+    checkBox.className="task-item__checkbox";
     editInput.type="text";
     editInput.className="task-item__input";
 
